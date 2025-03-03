0180f83c3865e0b05bd108d17436b3d97e73b635
Andrew Ivanov <andrus44ka@gamil.com>
2025-03-03 17:04:28 +0300

    Инициализация проекта

---
+++ src/main/java/org/example/boardgamestats/BoardGameStatsApplication.java
@@ -0,0 +1,13 @@
+package org.example.boardgamestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGameStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/boardgamestats/BoardGameStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boardgamestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
