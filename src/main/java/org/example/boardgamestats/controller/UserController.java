4c5b4bd567329421f58869bde07e036bdf3b501a
Andrew Ivanov <andrus44ka@gamil.com>
2025-03-05 17:55:20 +0300

    Добавлена метод поиска по id - getById с исключением, контроллером, сервисом и запросом

--- src/main/java/org/example/boardgamestats/controller/UserController.java
+++ src/main/java/org/example/boardgamestats/controller/UserController.java
@@ -2,7 +2,7 @@ package org.example.boardgamestats.controller;
 
 import org.example.boardgamestats.entity.UserEntity;
 import org.example.boardgamestats.exception.UserAlreadyExistException;
-import org.example.boardgamestats.repository.UserRepository;
+import org.example.boardgamestats.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +23,17 @@ public class UserController {
             return ResponseEntity.ok().body("Пользователь \"" + user.getName() + "\" успешно сохранен");
         } catch (UserAlreadyExistException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity getById(@RequestParam Long userId) throws UserNotFoundException {
+        try {
+            return ResponseEntity.ok().body("Пользователь c id: " + userId + " найден: " + userService.getById(userId).getName());
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
--- src/main/java/org/example/boardgamestats/service/UserService.java
+++ src/main/java/org/example/boardgamestats/service/UserService.java
@@ -2,6 +2,7 @@ package org.example.boardgamestats.service;
 
 import org.example.boardgamestats.entity.UserEntity;
 import org.example.boardgamestats.exception.UserAlreadyExistException;
+import org.example.boardgamestats.exception.UserNotFoundException;
 import org.example.boardgamestats.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,9 @@ public class UserService {
             throw new UserAlreadyExistException("Пользователь \"" + user.getName() + "\" уже существует");
         }
         return userRepository.save(user);
+    }
+
+    public UserEntity getById (Long id) throws UserNotFoundException {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Пользователь с id: " + id + " не найден"));
     }
 }
